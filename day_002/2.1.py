1cdd8fc53553d2031547a89147e70fe704960e63
kekkerslawl <braydj@gmail.com>
2020-12-01 10:39:28 -0600

    working through day2

---
+++ day_002/2.1.py
@@ -0,0 +1,9 @@
+# Data Types
+
+# String
+
+print("Hello"[-1])  # prints 'o'
+
+print("123" + "345")  # string
+
+print(123 + 345)  # integer
---
+++ day_002/2.2.py
@@ -0,0 +1,8 @@
+# num_char = len(input("What is your name? "))
+# new_num_char = str(num_char)
+# print("Your name is " + new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
+
+print(str(70) + float(100.3))
