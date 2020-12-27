334b1494ea23168837585f3a9397aa1b1a63b179
kekkerslawl <braydj@gmail.com>
2020-12-27 00:48:34 -0600

    working on day 7

--- day_007/Part1.py
+++ day_007/Part1.py
@@ -1,16 +1,25 @@
 import random
 word_list = ['aardvark', 'babboon', 'camel']
-display = ""
+display = []
 chosen_word = random.choice(word_list)
 print(f"pssst the chosen word is {chosen_word}")
 
+
+for _ in range(len(chosen_word)):
+    display+="_"
+
+
+print(display)
+
+
 player_guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
     if letter == player_guess:
-        display += letter
-    else:
-        display += "_"
+        display[position] = letter
+
 
 print(display)
 
